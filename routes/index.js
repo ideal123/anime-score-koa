f9d4c3139534e31d3900cc634a92ca7bfeb21abb
ideal123 <iseeucome@outlook.com>
2021-11-07 21:57:08 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require("koa-router")();
 
 router.get("/", async (ctx, next) => {
   await ctx.render("index", {
-    title: "Tencent CloudBase + Koa",
+    title: "Tencent CloudBase + Koa Anime",
   });
 });
 
